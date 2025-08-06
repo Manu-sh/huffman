8c44c9d70238302524e6776e4f9b3df596eb1483
Manu-Sh <s3gmentationfault@gmail.com>
2025-08-06 21:13:14 +0200

    Use stack

--- include/InverseSymbolTable.hpp
+++ include/InverseSymbolTable.hpp
@@ -29,22 +29,31 @@ struct InverseSymbolTable final { // faster than unordered_map
 
     private:
 
-        static constexpr uint32_t BUCKET_SIZE = pow2(15);
-        //static_assert(BUCKET_SIZE == 256, "ops");
-
         friend class SymbolTable;
         using Bucket = std::vector<std::pair<BitArray, uint8_t>>;
+        // TODO: questi sono 40 byte a botta, sicuramente i prefix-code sono lunghi al massimo 256
+        //  creare una classe/alias prefix-code rendere BitArray Template sulla lunghezza?
 
-        // prefix-code più lunghi sono meno frequenti, per cui bisogna cercare distribuire le collisioni su elementi lunghi
-        // anziché usare una distribuzione uniforme come x %256 ovvero x&(256-1)
-        // bisogna andare su una distribuzione pesata con pesi inversamente proporzionali alla lunghezza del prefix-code
-        // x % log2(prefix.bit_length), nonostante i molti hash provati questo è quello che si comporta meglio
+        // the maximum length of prefix codes, is 255.
+        // the reason is that every prefix free code (length in bits) never exceed the
+        // (number of symbols in the alphabet - 1) = 255 bits
+        static constexpr uint32_t BUCKET_SIZE = pow2(8); // 15 is the max val
+        static_assert(BUCKET_SIZE * sizeof(Bucket) < 786433, "stack smash for sure");
+
+        /*
+            prefix-code più lunghi sono meno frequenti, per cui bisogna cercare distribuire le collisioni su elementi lunghi
+            anziché usare una distribuzione uniforme come x %256 ovvero x&(256-1)
+            bisogna andare su una distribuzione pesata con pesi inversamente proporzionali alla lunghezza del prefix-code
+            x % log2(prefix.bit_length), nonostante i molti hash provati questo è quello che si comporta meglio
+
+            Longer prefix codes are less frequent, so we should try to distribute collisions across long elements.
+            Instead of using a uniform distribution like x %256 or x&(256-1)
+            We should use a weighted distribution with weights inversely proportional to the length of the prefix code.
+            x % log2(prefix.bit_length), despite the many hashes we've tried, this one performs best.
+        */
 
         static FORCED(inline) uint64_t prefix_code_hash(const BitArray &prefix_code) {
             return uint8_t(prefix_code.back_byte()) ^ prefix_code.bit_length();
-            //assert(prefix_code.bit_length());
-            //return (BUCKET_SIZE - prefix_code.bit_length()) % BUCKET_SIZE;
-            //return prefix_code.bit_length() & (BUCKET_SIZE - 1);
         }
 
         InverseSymbolTable() = delete;
@@ -64,5 +73,4 @@ struct InverseSymbolTable final { // faster than unordered_map
 
     protected:
         Bucket m_buckets[BUCKET_SIZE];
-
 };
