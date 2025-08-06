875b3a67dba199909ae2b17f4b416ae2c7a0279f
Manu-Sh <s3gmentationfault@gmail.com>
2025-08-06 19:55:03 +0200

    Inverted Symbol Table - bugfix, too much tryharding in middle of the night

--- include/InverseSymbolTable.hpp
+++ include/InverseSymbolTable.hpp
@@ -3,15 +3,20 @@
 #include <vector>
 #include <utility>
 #include <memory>
+#include <cstdlib>
 
 #include <bitarray/BitArray.hpp>
+
+static constexpr uint32_t pow2(uint8_t exp) {
+    return 2 << (exp - 1);
+}
 
 struct InverseSymbolTable final { // faster than unordered_map
 
     const uint8_t * find(const BitArray &huffman_code) const noexcept {
 
-        const uint16_t idx = prefix_code_hash(huffman_code) ^ 255;
-        const Bucket &bucket = buckets[idx];
+        const uint16_t idx = prefix_code_hash(huffman_code) & (BUCKET_SIZE-1);
+        const Bucket &bucket = m_buckets[idx];
 
         for (uint32_t i = 0, len = bucket.size(); i < len; ++i) {
             const auto &pair = bucket[i];
@@ -23,6 +28,10 @@ struct InverseSymbolTable final { // faster than unordered_map
     }
 
     private:
+
+        static constexpr uint32_t BUCKET_SIZE = pow2(15);
+        //static_assert(BUCKET_SIZE == 256, "ops");
+
         friend class SymbolTable;
         using Bucket = std::vector<std::pair<BitArray, uint8_t>>;
 
@@ -33,11 +42,13 @@ struct InverseSymbolTable final { // faster than unordered_map
 
         static FORCED(inline) uint64_t prefix_code_hash(const BitArray &prefix_code) {
             return uint8_t(prefix_code.back_byte()) ^ prefix_code.bit_length();
+            //assert(prefix_code.bit_length());
+            //return (BUCKET_SIZE - prefix_code.bit_length()) % BUCKET_SIZE;
+            //return prefix_code.bit_length() & (BUCKET_SIZE - 1);
         }
 
         InverseSymbolTable() = delete;
         InverseSymbolTable(std::shared_ptr<std::vector<BitArray>> shp_symbol_table) {
-
             const auto &symbol_table = *shp_symbol_table;
             for (uint64_t i = 0, len = symbol_table.size(); i < len; ++i) {
                 if (symbol_table[i].empty()) continue;
@@ -47,11 +58,11 @@ struct InverseSymbolTable final { // faster than unordered_map
         }
 
         void insert_unique(const BitArray &huffman_code, uint8_t symbol) {
-            const uint16_t idx = prefix_code_hash(huffman_code) ^ 255;
-            buckets[idx].emplace_back(huffman_code, symbol);
+            const uint16_t idx = prefix_code_hash(huffman_code) & (BUCKET_SIZE-1);
+            m_buckets[idx].emplace_back(huffman_code, symbol);
         }
 
     protected:
-        std::vector<Bucket> buckets{256};
+        Bucket m_buckets[BUCKET_SIZE];
 
 };
